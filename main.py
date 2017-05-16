f0386f29aa8d59409c4d1168b5b6ce60d31dbaba
Mieszko Chowaniec <mchowaniec@newstore.com>
2017-05-16 10:27:57 +0200

    iind dirty

---
+++ main.py
@@ -0,0 +1,41 @@
+# 1. Cellular automata with basic state memory.
+# 2. Cellular automata with Left/right shape symmetry.
+# 3. Cellular automata to form persistent hogher order entities which
+# will derive their properties from
+#
+
+
+
+
+
+import numpy as np
+# an automaton is a 4x4 array
+
+np.all
+
+
+class Res_extensa(object):
+    """
+    Base class for all enitites in the universe
+    Every enitity inherits that class.
+    """
+    pass
+
+
+class Atomic_cell(Res_extensa):
+    """
+    Basic, atomic cell. Undivisible. Atomos.
+    """
+    pass
+
+
+def compose_higher_order(entities):
+    """
+    """
+    if isinstance(enitity, Res_extensa):
+        #compose
+        return HigherOrder
+
+class HigherOrder(Res_extensa):
+    def __init__(self, entities):
+        self.entities = entities
